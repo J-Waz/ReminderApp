ca20d3d574f339268d8ab30be37ca5580f3b4872
J-Waz <jgw.jakegeorge@gmail.com>
2020-08-25 12:10:57 -0500

    add src, make comp, import comp, rendered fine

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/app'
 
 
 ReactDOM.render(
-    <div>Reminder Pro</div>, document.getElementById('root')
+    <App />, document.getElementById('root')
 )
